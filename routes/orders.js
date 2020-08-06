e83cab58256bbc1cace9d8b05465a3894b83fc1f
Patel Krushankant <patelkk1618@gmail.com>
2020-08-06 04:08:07 -0500

    Get single order routes

--- routes/orders.js
+++ routes/orders.js
@@ -33,5 +33,38 @@ router.get('/', (req, res) => {
         }).catch(err => res.json(err));
 });
 
+// Get Single Order
+router.get('/:id', async (req, res) => {
+    let orderId = req.params.id;
+    console.log(orderId);
+
+    database.table('orders_details as od')
+        .join([
+            {
+                table: 'orders as o',
+                on: 'o.id = od.order_id'
+            },
+            {
+                table: 'products as p',
+                on: 'p.id = od.product_id'
+            },
+            {
+                table: 'users as u',
+                on: 'u.id = o.user_id'
+            }
+        ])
+        .withFields(['o.id', 'p.title', 'p.description', 'p.price', 'p.image', 'od.quantity as quantityOrdered'])
+        .filter({'o.id': orderId})
+        .getAll()
+        .then(orders => {
+            console.log(orders);
+            if (orders.length > 0) {
+                res.json(orders);
+            } else {
+                res.json({message: "No orders found"});
+            }
+
+        }).catch(err => res.json(err));
+});
 
 module.exports = router;
